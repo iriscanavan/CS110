db5039aa44fb9b8cbd70c8c91184edfc3638b4fd
Iris Canavan <iriscanavan@Iriss-MacBook-Air.local>
2023-02-02 21:38:33 -0800

    Initial commit

---
+++ in_class/icp2.py
@@ -0,0 +1,39 @@
+import turtle
+turtle.showturtle()
+
+radius = 50
+decrement = 10
+
+#move
+turtle.penup()
+turtle.goto(0, radius)
+turtle.pendown()
+
+#draw dot
+turtle.dot()
+
+#move
+turtle.penup()
+turtle.goto(0, 0)
+turtle.pendown()
+
+#draw circle
+turtle.circle(radius)
+
+#move
+turtle.penup()
+turtle.goto(radius * 2 - decrement, 0)
+turtle.pendown()
+
+#draw circle
+turtle.circle(radius)
+
+#move
+turtle.penup()
+turtle.goto(-radius * 2 + decrement, 0)
+turtle.pendown()
+
+#draw circle
+turtle.circle(radius)
+
+turtle.done()
