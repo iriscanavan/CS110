c6f51a7d39d31bff5c88788735ee890a7526a264
Iris Canavan <iriscanavan@iriss-air.ds.usfca.edu>
2023-02-22 15:45:49 -0800

    Fix comments

--- lab02/hotdog.py
+++ lab02/hotdog.py
@@ -1,34 +1,32 @@
 # Iris Canavan, Section 3
 
-# Constants 
-HOTDOGS = 10 
-BUNS = 8 
+HOTDOGS = 10
+BUNS = 8
 
-# Variables 
 numAttend = int(input("How many people are attending the cookout? = "))
 numHotdogs = int(input("How many hot dogs per person? = "))
 Total = numAttend * numHotdogs
 
-# Minimum hotdog packages 
-minHotdogs = Total // HOTDOGS 
-if Total % HOTDOGS > 0: 
-	minHotdogs = minHotdogs + 1 
+# minimum hotdog packages
+minHotdogs = Total // HOTDOGS
+if Total % HOTDOGS > 0:
+	minHotdogs = minHotdogs + 1
 
-# Minimum bun packages 
-minBuns = Total // BUNS 
-if Total % BUNS > 0: 
-	minBuns = minBuns + 1 
+# minimum bun packages
+minBuns = Total // BUNS
+if Total % BUNS > 0:
+	minBuns = minBuns + 1
 
-# Hotdog remainder 
-minHotdogs = Total // HOTDOGS 
-if Total % HOTDOGS > 0: 
-	minHotdogs = minHotdogs + 1 
+# hotdog remainder
+minHotdogs = Total // HOTDOGS
+if Total % HOTDOGS > 0:
+	minHotdogs = minHotdogs + 1
 remHotdogs = (HOTDOGS * minHotdogs - Total)
 
-# Hotdog bun remainder 
-minBuns = Total // BUNS 
-if Total % BUNS > 0: 
-	minBuns = minBuns + 1 
+# hotdog bun remainder
+minBuns = Total // BUNS
+if Total % BUNS > 0:
+	minBuns = minBuns + 1
 remBuns = (BUNS * minBuns - Total)
 
 print("The minimum number of packages of hot dogs required = ", minHotdogs)
