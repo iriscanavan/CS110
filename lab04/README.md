d44fc2ff0a8550bb379fe9ba1beb3d3bbf0fcdc5
Iris Canavan <iriscanavan@iriss-air.ds.usfca.edu>
2023-04-02 11:14:25 -0700

    Add lab04

---
+++ lab04/README.md
@@ -0,0 +1,23 @@
+# Lab04
+
+Program to manage an individual's address book. The user can add new contacts,
+print the entire address book, search for a contact, delete a contact,
+and exit the program.
+
+## Author
+
+[Iris Canavan](https://github.com/iriscanavan/cs110)\
+Student ID: 20679460
+
+## Problems
+
+It was difficult to figure out how to delete contacts and to print out the
+entire contacts information when a user searched.
+
+## Resources
+
+I used the slides from class or looked up any other questions I had.
+
+## Teamwork
+
+Lauren Jardiolin
---
+++ lab04/addressbook.py
@@ -0,0 +1,117 @@
+# Iris Canavan, Section 3
+
+def main():
+	fmenu()
+	choice = menu_choice
+	if choice == "1":
+		fprint_address_book()
+	elif choice == "2":
+		fsearch_contact()
+	elif choice == "3":
+		fadd_contact()
+	elif choice == "4":
+		fdelete_contact()
+	elif choice == "5":
+		again = input("Do you want to continue? (y/n): ")
+		if again == "y":
+			fmenu()
+		while again != "y" and again != "n":
+			again = input("Do you want to continue? (y/n): ")
+
+def fmenu():
+	print("----MENU----------------------")
+	print("    Print Address Book (1)")
+	print("    Search Contact (2)")
+	print("    Add New Contact (3)")
+	print("    Delete Contact (4)")
+	print("    Quit (5)")
+	global menu_choice
+	menu_choice = input("Enter your choice: ")
+	while menu_choice != "1" and menu_choice != "2" and menu_choice != "3" and menu_choice != "4" and menu_choice != "5":
+		print("Invalid input. Please try again.")
+		menu_choice = input("Enter your choice: ")
+	return menu_choice
+
+def fprint_address_book():
+	file = ffile_open("contacts.txt", "r")
+	print(file.read())
+	file.close()
+
+def fsearch_contact():
+	search_contact = input("Name of contact you are searching for: ")
+	file = ffile_open("contacts.txt", "r")
+	line_number = 1
+	boolFound = False
+	for line in file:
+		if line_number == 1:
+			spaces = line.rstrip()
+		elif line_number == 2:
+			name = line.rstrip()
+			if line.rstrip() == search_contact:
+				boolFound = True
+		elif line_number == 3:
+			address = line.rstrip()
+		elif line_number == 4:
+			zipcode = line.rstrip()
+			line_number = 0
+		if boolFound and line_number == 0:
+			print(spaces)
+			print(name)
+			print(address)
+			print(zipcode)
+			return
+		line_number += 1
+	file.close()
+	if not boolFound:
+		print(search_contact, "not found in address book")
+
+def fadd_contact():
+	name = input("Enter name: ")
+	address = input("Enter street address: ")
+	location = input("Enter city, state and zipcode: ")
+	file = ffile_open("contacts.txt", "a")
+	file.write("-" * 30)
+	file.write("\n")
+	file.write(name)
+	file.write("\n")
+	file.write(address)
+	file.write("\n")
+	file.write(location)
+	file.write("\n")
+	file.close()
+
+def fdelete_contact():
+	delete_contact = input("Name of contact you want to delete from the address book: ")
+	file = ffile_open("contacts.txt", "r")
+	directory = ""
+	name = ""
+	line_number = 1
+	for line in file:
+		if line_number == 1:
+			dash_line = line
+		elif line_number == 2:
+			name = line
+		elif line_number == 3:
+			address = line
+		elif line_number == 4:
+			location = line
+			line_number = 0
+		if delete_contact != name.rstrip() and line_number == 0:
+			directory += space
+			directory += name
+			directory += address
+			directory += location
+		line_number += 1
+	file.close()
+	file = ffile_open("contacts.txt", "w")
+	file.write(directory)
+	file.close()
+
+def ffile_open(fileName, mode):
+	try:
+		file_open = open("contacts.txt", mode)
+		return file_open
+	except FileNotFoundError:
+		print("File not found")
+
+main()
