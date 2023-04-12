6048fff7a0ebde4496ca20f9892ef8b3e41d2656
Iris Canavan <iriscanavan@iriss-air.ds.usfca.edu>
2023-04-12 00:02:06 -0700

    Add cumulative cases

--- project02/Day.py
+++ project02/Day.py
@@ -1,10 +1,20 @@
 class Day:
-	__date = None
-	__cumulativeCases = None
+	__date: int
+	__cumulativeCases: int
+	__newDailyCases: int
 
 	def __init__(self, date, cumulativeCases):
 		self.__date = date
 		self.__cumulativeCases = cumulativeCases
+
+	def getCumulativeCases(self):
+		return int(self.__cumulativeCases)
+
+	def setNewDailyCases(self, newDailyCases):
+		self.__newDailyCases = newDailyCases
+
+	def getNewDailyCases(self):
+		return int(self.__newDailyCases)
 
 	def writeData(self, fpath):
 		file = open(fpath, "a")
--- project02/project2.py
+++ project02/project2.py
@@ -21,6 +21,13 @@ def main():
 	for i in range(0, 3):
 		daily_data += read_input_file(FILE_PATH_INPUT[i])
 
+	# Calculate the new daily cases by subtracting yesterday's cumulative
+	# cases from today's cumulative cases. The first day (daily_data[0])'s
+	# new daily cases should always be the same as its cumulative cases.
+	daily_data[0].setNewDailyCases(daily_data[0].getCumulativeCases())
+	for i, value in enumerate(daily_data[1:], 1):
+		daily_data[i].setNewDailyCases(daily_data[i].getCumulativeCases() - daily_data[i - 1].getCumulativeCases())
+
 	for i in range(len(daily_data)):
 		daily_data[i].writeData(FILE_PATH_OUTPUT)
 
