5bb6b012948163e914d9ac53e6f55dacb1e72dfb
Iris Canavan <iriscanavan@iriss-air.ds.usfca.edu>
2023-05-10 23:56:28 -0700

    Clean up

--- project04/stocks.py
+++ project04/stocks.py
@@ -6,12 +6,9 @@ def main():
 	print(response)
 	print("Length of response: ", len(response))
 
-
 def get_stock(symbol):
 	stock = requests.get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&datatype=json&apikey=VPVN3LZF0ZVPNXB2&symbol=" + symbol)
 	print(stock_response)
 	return stock.text
 
 main()
-
-
